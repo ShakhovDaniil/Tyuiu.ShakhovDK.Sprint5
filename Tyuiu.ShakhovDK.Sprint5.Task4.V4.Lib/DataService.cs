bb7f86d3c2276dac0109df740e60c8573087091d
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-11-29 23:54:51 +0500

    Task4

---
+++ Tyuiu.ShakhovDK.Sprint5.Task4.V4.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ShakhovDK.Sprint5.Task4.V4.Lib
+{
+    public class DataService : ISprint5Task4V4
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string x = File.ReadAllText(path);
+            double y =  Math.Round(0.25 * Convert.ToDouble(x) / Math.Cos(Convert.ToDouble(x)), 3);
+            return y;
+        }
+    }
+}
