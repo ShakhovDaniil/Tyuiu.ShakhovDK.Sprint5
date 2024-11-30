d58130fe00dea34c1cba25d02bd6f2bc19246d09
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-11-30 06:25:22 +0500

    Task6

---
+++ Tyuiu.ShakhovDK.Sprint5.Task6.V29.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ShakhovDK.Sprint5.Task6.V29.Lib
+{
+    public class DataService : ISprint5Task6V29
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string data = File.ReadAllText(path);
+            string[] f_array = data.Split(' ');
+            int res = 0;
+            foreach (string s in f_array)
+            {
+                if (s.Length == 7)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShakhovDK.Sprint5.Task6.V29.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.ShakhovDK.Sprint5.Task6.V29.Lib;
+namespace Tyuiu.ShakhovDK.Sprint5.Task6.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V29.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool test_res = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, test_res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V29.txt";
+            DataService ds = new DataService();
+            int wait = 3;
+            int res = ds.LoadFromDataFile(path);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
